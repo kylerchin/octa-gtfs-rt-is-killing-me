e8ab32c87295dc3c6a04bd3e3f94dcef0ecd1a3a
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-08-08 15:45:17 -0700

    Update test-octa.py

--- test-octa.py
+++ test-octa.py
@@ -1,6 +1,18 @@
 from google.transit import gtfs_realtime_pb2
 import requests
 
+print("testing Long Beach")
+
+feed = gtfs_realtime_pb2.FeedMessage()
+response = requests.get('https://lbtgtfs.lbtransit.com/TMGTFSRealTimeWebService/Vehicle/VehiclePositions.pb')
+feed.ParseFromString(response.content)
+for entity in feed.entity:
+  if entity.HasField('vehicle_position'):
+    print(entity.vehicle_position)
+
+
+print("testing octa")
+
 feed = gtfs_realtime_pb2.FeedMessage()
 response = requests.get('https://api.goswift.ly/real-time/octa/gtfs-rt-vehicle-positions')
 feed.ParseFromString(response.content)
